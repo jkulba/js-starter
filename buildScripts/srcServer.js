95d21bae34ffdd2482b5037cd5c6a4d3e98b5fcf
James Kulba <James.Kulba@thermofisher.com>
2017-09-08 15:37:42 -0700

    + added linting configuration

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,8 +1,10 @@
-import express from'express';
-import path from'path';
-import open from'open';
+import express from 'express';
+import path from 'path';
+import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
+
+/* eslint-disable no-console */
 
 const port = 3000;
 const app = express();
@@ -13,11 +15,11 @@ app.use(require('webpack-dev-middleware')(compiler, {
   publicPath: config.output.publicPath
 }));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-app.listen(port, function(err) {
+app.listen(port, function (err) {
   if (err) {
     console.log(err);
   } else {
